d8cfe3d1fb7d8bb9db4982062bafd1db36ad8e32
Tyler <tyler.s9@proton.me>
2026-01-25 17:12:27 -0500

    Added more details to student form

--- src/com/hello/Student.java
+++ src/com/hello/Student.java
@@ -7,6 +7,11 @@ import javax.faces.bean.RequestScoped;
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String age;
+	private String dob;
+	private String email;
+	private String major;
+	private String year;
 	
 	// no-arg constructor
 	public Student() {
@@ -28,5 +33,45 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
 	}
 }
